561651e7c67073925d3f4710f58867a05c881917
Caspar Owner <casparowner@gmail.com>
2026-09-24 20:33:31 +0530

    Time: 10 ms (30.43%) | Memory: 12.4 MB (21.74%) - LeetSync

---
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def smallestIndex(self, nums):
+        self.nums = nums
+
+        for num_location in range(len(nums)):
+
+            location = nums[num_location]
+            if len(str(nums[num_location])) > 1:
+                location = 0
+                for x in str(nums[num_location]):
+                    location += int(x)
+            
+
+            if location == num_location:
+                return num_location
+            
+        return -1
+        
